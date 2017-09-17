b814701d3af2801c867a64b6ac984b297045dbe6
MaxReginya <maksimr1998@mail.ru>
2017-09-17 20:51:27 +0300

    Draw solid rectangle that will be relocated by mouse and keyboard

--- WINAPI_Graphics/WINAPI_Graphics.cpp
+++ WINAPI_Graphics/WINAPI_Graphics.cpp
@@ -3,12 +3,16 @@
 #include "WINAPI_Graphics.h"
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CreateRect(HDC, RECT, HBRUSH);
+
+const int RECT_HEIGHT = 100, RECT_WIDTH = 300;
+const int R = 100, G = 200, B = 200;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int
 	nCmdShow)
 {
 	MSG msg;
-	
+	WndClass mainWnd(L"A WINAPI_Graphics application", hInstance, nCmdShow, WndProc);
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
@@ -20,13 +24,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	HDC hDC;	
 	PAINTSTRUCT ps;
+	static RECT rect = { 0, 0, RECT_WIDTH, RECT_HEIGHT };
+	HBRUSH brush = (HBRUSH)CreateSolidBrush(RGB(R, G, B));
 
 	switch (uMsg)
 	{		
 	case WM_PAINT:
 	{		
+		InvalidateRect(hWnd, 0, TRUE);
 		hDC = BeginPaint(hWnd, &ps);		
-		
+		CreateRect(hDC, rect, brush);
 		EndPaint(hWnd, &ps);
 		break;
 	}	
@@ -41,3 +48,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
+void CreateRect(HDC hDC, RECT rect, HBRUSH brush)
+{
+	Rectangle(hDC, rect.left, rect.top, rect.right, rect.bottom);
+	FillRect(hDC, &rect, brush);
+}
